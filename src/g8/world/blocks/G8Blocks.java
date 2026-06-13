1a4cd248485bd0589dc9b70f0e1f54ce5fade3c7
jstation <sophiabeachanel@gmail.com>
2026-06-13 17:00:26 +0800

    Update and rename g8b.java to G8Blocks.java

--- src/g8/world/blocks/G8Blocks.java
+++ src/g8/world/blocks/G8Blocks.java
@@ -3,7 +3,7 @@ package game8.blocks
 import mindustry.world.Blocks;
 import arc.math.geom.Vec2;
 
-public class g8b extends Blocks {
+public class G8Blocks extends Blocks {
   private Vec2 press;
   private boolean pressreq;
   private float pressreqfluct;
