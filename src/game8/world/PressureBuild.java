1fdd057ed00613065516173723a2ed53ae06624c
jstation <sophiabeachanel@gmail.com>
2026-07-03 22:38:30 +0800

    off

--- src/game8/world/PressureBuild.java
+++ src/game8/world/PressureBuild.java
@@ -1,10 +1,14 @@
 package game8.world;
 
 import arc.struct.Seq;
+import arc.struct.IntSet;
 import mindustry.gen.Building;
 
 public interface PressureBuild {
-    Seq<Building> System();
+    Seq<Building> System;
+    Seq<Building> System() {
+        return System;
+    };
     float Pressure() {
         private float SysTotalPressure;
         private float SysFinalPressure;
@@ -12,4 +16,31 @@ public interface PressureBuild {
         return SysFinalPress;
     }
     float Volume();
+    Seq<Building> updateSystem() {
+        IntSet tracked = new IntSet();
+        Seq<Building> isTracking = new Seq<Building>();
+        Seq<Building> hasAccepted = new Seq<Building>();
+
+        isTracking.add(this.proximity);
+
+        while(!isTracking.isEmpty) { // if queue is not empty
+            for(Building current : isTracking) { // loop through each block in queue
+                if(tracked.add(current.pos) /** adds this position into tracked */ && current.block.ConnectPressureSystems == true) { /** checks if this build is a build to return */
+                    hasAccepted.add(current);
+                    isTracking.add(current.proximity);
+                }
+                isTracking.remove(current);
+            }
+        } // once queue is empty...
+
+        // now that flood-fill is finished...
+
+        for(Building current : hasAccepted) {
+            current.System.clear();
+            current.System.add(hasAccepted);
+        }
+
+        return hasAccepted;
+    }
+    public boolean IsPressureBuild = true;
 }
---
+++ src/game8/world/blocks/PressureBlock.java
@@ -0,0 +1,5 @@
+package game8.world.blocks;
+
+public interface PressureBlock {
+    public boolean ConnectPressureSystems = false;
+}
