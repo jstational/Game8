9076ec6363bd133137e0deb9663ca44c61afbc41
jstation <sophiabeachanel@gmail.com>
2026-07-09 22:55:17 +0800

    qw

--- src/game8/logic/PLExecutor.java
+++ src/game8/logic/PLExecutor.java
@@ -5,6 +5,5 @@ import game8.logic.*;
 // pressurelogic executor
 public class PLExecutor {
     public PLVar[] vars = {};
-
-    public void next() {}
+    public PLInstruction[] instructs = {};
 }
--- src/game8/world/blocks/defense/BulletMine.java
+++ src/game8/world/blocks/defense/BulletMine.java
@@ -2,9 +2,11 @@ package game8.world.blocks.distribution.items;
 
 import mindustry.world.Block;
 import mindustry.gen.Building;
-import mindustry.gen.Bullet;
+import mindustry.entities.bullet.*;
 
 public class BulletMine extends Block {
+    public BulletType bullet;
+
     public BulletMine(String name) {
         super(name);
     }
