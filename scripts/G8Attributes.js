7a41b154175094bfc8685e965e4c8177c3fb6c24
jstation <sophiabeachanel@gmail.com>
2026-07-14 21:45:09 +0800

    s

---
+++ scripts/G8Attributes.js
@@ -0,0 +1,8 @@
+const g8attributes = {
+    helloWorld: attribute.add("helloWorld"),
+    /* DUDE JS WHAT IS THIS im crying help */
+};
+
+/* whatever you assign to module.exports is what gets returned when another file calls require() on your file. */
+/* essentially an artesian object? */
+module.exports = g8attributes;
--- src/game8/world/BaseBlock.java
+++ src/game8/world/BaseBlock.java
@@ -1,12 +1,12 @@
 package game8.world;
 
 import mindustry.world.Block;
-import arc.struct.Seq;
+import arc.struct.*;
 import mindustry.gen.Building;
 
 /** base block for game8 */
 public class BaseBlock extends Block {
-    public float minPressure, maxPressure, volume, minGenPressure, maxGenPressure, minConsumePressure, maxConsumePressure = 0f;
+    public float minPressure, maxPressure, volume, minGenPressure, maxGenPressure, minConsumePressure, maxConsumePressure, genInterval, consumeInterval = 0f;
     public boolean hasPressure, producePressure, connectPressureSystems, consumePressure, startConsumeMin, startProduceMin = false;
 
     public BaseBlock(String name) {
@@ -16,5 +16,54 @@ public class BaseBlock extends Block {
     public class BaseBuild extends Building {
         public boolean consumingMax, genMax, syncedToSystem;
         public Seq<Building> pressureSystem;
+
+        public Seq<Building> getSystem() {
+            Building curt;
+            Seq<Building> processed;
+            Seq<Building> willt;
+            IntSet processedpos;
+
+            return null;
+        }
+
+        public float getConsumePressure() {
+            return consumingMax ? this.block.maxConsumePressure : this.block.minConsumePressure;
+        }
+
+        public float getGenPressure() {
+           return genMax ? this.block.maxGenPressure : this.block.minGenPressure;
+        }
+
+        public float getMaxPressure() {
+            return this.block.maxPressure;
+        }
+
+        public float getMinPressure() {
+            return this.block.minPressure;
+        }
+
+        public float getVolume() {
+            return this.block.volume;
+        }
+
+        public boolean hasPressure() {
+            return this.block.hasPressure;
+        }
+
+        public boolean producePressure() {
+            return this.block.producePressure;
+        }
+
+        public float getGenInterval() {
+            return this.block.genInterval;
+        }
+
+        public float getConsumeInterval() {
+            return this.block.consumeInterval;
+        }
+ 
+        public void setSystem() {}
+
+        public void redefSystem() {}
     }
 }
