d380babae225598e9f11a7997e0bad2df6d2dde9
jstation <sophiabeachanel@gmail.com>
2026-07-17 16:04:58 +0800

    small chang yet aain

--- src/game8/type/TurretSegmentSegment.java
+++ src/game8/type/TurretSegmentSegment.java
@@ -4,11 +4,12 @@ import arc.graphics.g2d.*;
 import arc.struct.*;
 import mindustry.type.*;
 import mindustry.entities.bullet.*;
+import mindustry.content.*;
 
 public class TurretSegmentSegment {
     public float x, y, scale = 0f;
     public Seq<TurretSegmentSegment> turretSegments = new Seq<TurretSegmentSegment>();
-    public BulletType bullet = new BulletType();
+    public BulletType bullet = Bullets.placeholder;
 
     public TurretSegmentSegment() {}
 }
--- src/game8/world/blocks/defense/BulletMine.java
+++ src/game8/world/blocks/defense/BulletMine.java
@@ -3,9 +3,10 @@ package game8.world.blocks.distribution.items;
 import game8.world.*;
 import mindustry.gen.Building;
 import mindustry.entities.bullet.*;
+import mindustry.content.*;
 
 public class BulletMine extends BaseBlock {
-    public BulletType bullet;
+    public BulletType bullet = Bullets.placeholder;
 
     public BulletMine(String name) {
         super(name);
