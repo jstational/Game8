09f8757ceea17066aa9baf3a453a01b9736df089
jstation <sophiabeachanel@gmail.com>
2026-07-17 18:41:54 +0800

    df

--- src/game8/type/TurretSegmentSegment.java
+++ src/game8/type/TurretSegmentSegment.java
@@ -5,11 +5,20 @@ import arc.struct.*;
 import mindustry.type.*;
 import mindustry.entities.bullet.*;
 import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.part.*;
 
 public class TurretSegmentSegment {
     public float x, y, scale = 0f;
     public Seq<TurretSegmentSegment> turretSegments = new Seq<TurretSegmentSegment>();
     public BulletType bullet = Bullets.placeholder;
+    public Effect ejectEffect = Fx.none;
+    public Effect shootEffect = Fx.none;
+    /** im gonna try to make this ACTUALLY work. */
+    public boolean flipSprite = false;
+    /** shootorder goes forward */
+    public int shootOrder = 0;
+    public DrawPart drawPart = new DrawPart();
 
     public TurretSegmentSegment() {}
 }
