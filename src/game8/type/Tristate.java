46bc90a1f4975c4f645db5286011cd76459d68cd
jstation <sophiabeachanel@gmail.com>
2026-07-19 16:42:17 +0800

    a

--- src/game8/type/Tristate.java
+++ src/game8/type/Tristate.java
@@ -11,22 +11,6 @@ public class Tristate {
         this(a, b, false);
     }
 
-    public Tristate(int val) {
-        this(val > 0, val == 2, false);
-    }
-
-    public Tristate(int val, boolean balanced) {
-        this(val > (balanced ? -1 : 0), val == (balanced ? 1 : 2), balanced);
-    }
-
-    public Tristate(float val) {
-        this(Math.floor(val) > 0f, Math.floor(val) == 2f, false);
-    }
-
-    public Tristate(float val, boolean balanced) {
-        this(Math.floor(val) > (balanced ? -1f : 0f), val == (balanced ? 1f : 2f), balanced);
-    }
-
     public Tristate(boolean a, boolean b, boolean balanced) {
         this.valA = a;
         this.valB = b;
