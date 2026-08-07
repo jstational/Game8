e8c4375799fdfc830c17781786e845707d24f01c
jstation <sophiabeachanel@gmail.com>
2026-08-07 16:58:05 +0800

    apple8.3: apple factory 1.3/2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,60 @@
 import org.gradle.jvm.tasks.Jar
-import java.io.file.Files
-import java.io.file.Paths
+import java.nio.file.Files
+import java.nio.file.Paths
 
 plugins {
     java
 }
 
 // this repo contains code that will be merged into src
-val otherURL = "https://github.com/jstational/g8tf.git"
-val otherR = "main"
-val otherDir = "src"
+object apply {
+    val fromURL = "https://github.com/jstational/g8tf.git"
+    val fromBranch = "main"
+    val fromDir = "src/"
 
-version = "1.0"
+    // apply here
+    val toDir = "src/"
+}
 
-sourceSets.main.java.srcDirs("src/java")
+// Mindustry version to depend on.
+// Valid values:
+// - latest: depend on the latest release of mindustry
+// - be: depend on the very latest commit of mindustry
+// - v<number>: depend on a specific commit
+val mindustryVersion = "latest"
+val projectVersion = "0"
+
+val javaVersion = "17"
+
+object dirs {
+    val coreDir = "core"
+
+    object source {
+        val sourceDir = dirs.coreDir + "/src"
+
+        val javascriptSourceDir = dirs.source.sourceDir + "/javascript"
+        val javaSourceDir = dirs.source.sourceDir + "/java"
+    }
+
+    object assets {
+        val assetDir = dirs.coreDir + "/assets"
+    }
+}
+
+sourceSets.main.get().java.srcDirs(dirs.source.javaSourceDir)
 
 // this was converted to kotlin cause im more familiar with it
 // i might learn Groovy for my tech stack sometime
 
-// always assumes src/javascript exists, if a refactor changes src/javascript's path, change this too
+// always assumes src/javascript exists, if a refactor changes src/javascript's path, change srcDirs.javascriptSourceDir
 tasks.register("rename") {
     doFirst {
-        val target = layout.projectDirectory.dir("src/javascript").asFile.toPath()
-        val new = layout.projectDirectory.dir("src/scripts").asFile.toPath()
+        val jstarget = layout.projectDirectory.dir(dirs.source.javascriptSourceDir).asFile.toPath()
+        val jsnew = layout.projectDirectory.dir(dirs.source.sourceDir + "/scripts").asFile.toPath()
 
-        Files.move(target, new)
+        if(Files.exists(jstarget)) {
+            Files.move(jstarget, jsnew)
+        }
     }
 }
 
@@ -72,44 +102,42 @@ repositories {
 }
 
 java {
-    targetCompatibility = JavaVersion.VERSION_17
-    sourceCompatibility = JavaVersion.VERSION_17
+    val ver = if(javaVersion == "latest") JavaVersion.values().last() else if(javaVersion == "mindustryJavaVersion") JavaVersion.VERSION_17 else try {
+        JavaVersion.toVersion(javaVersion)
+    } catch(e: IllegalArgumentException) {
+        JavaVersion.VERSION_17
+    }
+
+    targetCompatibility = ver
+    sourceCompatibility = ver
 }
 
-// Mindustry version to depend on.
-// Valid values:
-// - latest: depend on the latest release of mindustry
-// - be: depend on the very latest commit of mindustry
-// - v<number>: depend on a specific commit
-val mindustryVersion = "latest"
 val isWindows = System.getProperty("os.name").toLowerCase().contains("windows")
 val sdkRoot = System.getenv("ANDROID_HOME") ?: System.getenv("ANDROID_SDK_ROOT")
 
 dependencies {
-    compileOnly(
-        if(mindustryVersion == "be") "Anuken:MindustryBuilds:latest" else "Anuken:Mindustry:$mindustryVersion"
-    )
+    compileOnly(if(mindustryVersion == "be") "Anuken:MindustryBuilds:latest" else "Anuken:Mindustry:$mindustryVersion")
 }
 
-val jarAndroid = tasks.register("jarAndroid") {
+tasks.register("jarAndroid") {
     dependsOn("jar")
     val projectName = project.name
     val noAndroidSDK: String = "No valid Android SDK found. Ensure that ANDROID_HOME is set to your Android SDK directory."
     val noAndroidJar: String = "No android.jar found. Ensure that you have an Android platform installed."
 
     doLast {
-        if(sdkRoot.isNullOrEmpty() || !new File(sdkRoot).exists()) throw new GradleException(noAndroidSDK)
+        if(sdkRoot.isNullOrEmpty() || !File(sdkRoot).exists()) throw GradleException(noAndroidSDK)
 
-        val platformRoot = File("$sdkRoot/platforms/").listFiles().sortedDescending.find {
+        val platformRoot = File("$sdkRoot/platforms/").listFiles().sortedDescending().toList().find {
                 File(it, "android.jar")
             }
 
-        if(!platformRoot) throw new GradleException(noAndroidJar)
+        if(platformRoot == null) throw GradleException(noAndroidJar)
 
         //collect dependencies needed for desugaring
-        val dependencies = (configurations.get().files + configurations.runtimeClasspath.get().files + File(platformRoot, "android.jar")).joinToString(" ") { "--classpath ${it.path}" }
+        val dependencies = (configurations.compileClasspath.get().files + configurations.runtimeClasspath.get().files + File(platformRoot, "android.jar")).joinToString(" ") { "--classpath ${it.path}" }
 
-        val d8 = if isWindows; "d8.bat" else "d8"
+        val d8 = if(isWindows) "d8.bat" else "d8"
 
         //dex and desugar files - this requires d8 in your PATH
         val commands = "$d8 $dependencies --min-api 14 --output ${projectName}Android.jar ${projectName}Desktop.jar"
@@ -119,20 +147,22 @@ val jarAndroid = tasks.register("jarAndroid") {
 }
 
 tasks.jar {
+    dependsOn("rename")
+
     archiveFileName = "${project.name}Desktop.jar"
+
+    from(dirs.source.sourceDir + "/") {
+        include("scripts/**")
+    }
 
     from({
         configurations.runtimeClasspath.get().map {
-            if it.isDirectory();c it else zipTree(it)
+            if(it.isDirectory()) it else zipTree(it)
         }
     })
 
-    from("assets/") {
+    from(dirs.assets.assetDir + "/") {
         include("**")
-    }
-
-    from("src/") {
-        include("scripts")
     }
 
     from(projectDir) {
@@ -142,15 +172,19 @@ tasks.jar {
 
 tasks.register<Jar>("deploy") {
     val projectName = project.name
-    dependsOn(jarAndroid)
-    dependsOn(jar)
+    dependsOn("jarAndroid")
+    dependsOn("jar")
     archiveFileName.set("${projectName}.jar")
 
     from({
-        listOf(zipTree("build/libs/${projectName}Desktop.jar"), zipTree("build/libs/${projectName}Android.jar"))
+        listOf([
+            zipTree("build/libs/${projectName}Desktop.jar"),
+            zipTree("build/libs/${projectName}Android.jar")
+        ])
     })
 
     doLast {
         delete("build/libs/${projectName}Android.jar")
+        delete("build/libs/${projectName}Desktop.jar")
     }
 }
